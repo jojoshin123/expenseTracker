30e79e910c4fa15289a6c82557c7fd02e51dcc11
Jonah Shin <jonahshin@jonahs-mbp.lan>
2021-05-05 23:15:27 -0700

    Update expense and add SQL cmds

--- src/main/java/model/Expense.java
+++ src/main/java/model/Expense.java
@@ -22,4 +22,70 @@ public class Expense {
 
     @ManyToOne
     private Category category;
+
+
+    public Expense() {
+    }
+
+    public Expense(Long id, Instant expensedate, String descript, double price, User user, Category category) {
+        this.id = id;
+        this.expensedate = expensedate;
+        this.descript = descript;
+        this.price = price;
+        this.user = user;
+        this.category = category;
+    }
+
+    //Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setExpensedate(Instant expensedate) {
+        this.expensedate = expensedate;
+    }
+
+    public void setDescript(String descript) {
+        this.descript = descript;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+
+    //Getters
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getExpensedate() {
+        return expensedate;
+    }
+
+    public String getDescript() {
+        return descript;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
 }
